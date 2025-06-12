44ae42a6f65189046c61835187fc396993211baf
minji_105 <ajhjmj9@naver.com>
2025-06-12 13:57:00 +0900

    페이지 컴포넌트 생성 및 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useEffect } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMultiplePokemonById } from "./RTK/thunk";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Search from "./pages/Search";
+import Favorite from "./pages/Favorite";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,9 +18,14 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1 className="text-lg text-center">hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Main />} />
+        <Route path={"/detail"} element={<Detail />} />
+        <Route path={"/search"} element={<Search />} />
+        <Route path={"/favorite"} element={<Favorite />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
