6280b475002c71776a1628df7e9d37df25da143b
minji_105 <ajhjmj9@naver.com>
2025-06-14 03:45:48 +0900

    tailwind 버전 4로 업그레이트 & 상세 이미지 flip 애니메이션 효과 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import FavoriteButton from "../components/FavoriteButton";
 
 function Detail() {
-  const [isFront, setIsFront] = useState(true);
+  const [flipped, setFlipped] = useState(false);
   const { pokemonId } = useParams();
   const pokemon = useSelector(selectPokemonById(Number(pokemonId)));
 
@@ -13,15 +13,29 @@ function Detail() {
     <div className="relative w-4/5 m-auto border-2 border-black grid md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr]">
       <FavoriteButton pokemonId={Number(pokemonId)} position="top-4 left-4" />
       <div className="basis-1/2 p-12 flex flex-col items-center border-black outline outline-1">
-        <img className="w-full" src={isFront ? pokemon.front : pokemon.back} />
+        <div className="w-full perspective-800">
+          <div
+            className={`relative w-full transition-transform duration-500 transform-style preserve-3d transfrom-3d ${
+              flipped ? "rotate-y-180" : ""
+            }`}
+          >
+            <img className="w-full backface-hidden" src={pokemon.front} />
+            <img
+              className="w-full absolute top-0 backface-hidden rotate-y-180"
+              src={pokemon.back}
+            />
+          </div>
+        </div>
+
         <button
           className="w-min bg-black py-2 px-4 text-white text-2xl rounded-3xl border-2 border-black
                       hover:bg-transparent hover:text-black active:bg-yellow-300"
-          onClick={() => setIsFront((prev) => !prev)}
+          onClick={() => setFlipped((prev) => !prev)}
         >
           click
         </button>
       </div>
+
       <div className="basis-1/2 px-4 outline outline-1">
         <p className="border-b-2 border-black py-4 text-xl font-semibold">
           전국: <span className="text-2xl font-thin">{pokemon.id}</span>
